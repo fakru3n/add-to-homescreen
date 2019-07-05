5a9a3129553f7568c7883b0527b228e0623f6efd
Syed S <syed.s@adityabirla.bank>
2019-07-05 15:30:04 +0530

    Heroku Commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 6000;
-const dirPath = express.static(path.join(__dirname, '/demos'));
+const port = process.env.PORT || 8000;
+const dirPath = express.static(path.join(__dirname, '/demos/simple/'));
 app.use('/', dirPath);
 
 app.get('/', (req, res) => {
-    res.sendFile('/add-event/index.html')
+    res.sendFile('index.html')
 })
 app.use('/check', dirPath);
 app.get('/check', (req, res) => {
